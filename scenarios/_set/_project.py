253fb38392a3808cb14adc3004441aea805d3df4
Leonid Petukhov <Petukhov.L.work@yandex.ru>
2025-02-07 18:13:48 +0300

    Настройка проектов

---
+++ scenarios/_set/_project.py
@@ -0,0 +1,17 @@
+if __name__ == "__main__":
+	import sys
+	sys.path.insert(0, "..\\..\\")
+from resources.rpa._RPAWeb import  DefaultScript, DefaultSubscript
+
+''' Настройки тестов проекта '''
+class ProjectTest (DefaultScript):
+
+    def _temp(self):
+        return
+
+
+''' Настройки тестов проекта '''
+class ProjectSet (DefaultSubscript):
+
+    def _temp(self):
+        return
